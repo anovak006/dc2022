266719b728406ea89ccb800e84a0643167991358
Albert Novak <albert.novak@carnet.hr>
2022-05-18 01:15:16 +0200

    Poveži RESTful API sa sudionicima u bazi podataka

--- dcapi/routers/sudionik.py
+++ dcapi/routers/sudionik.py
@@ -9,6 +9,10 @@ from typing import List
 from dcapi.helpers import get_db
 from dcdb.api import (
     kreiraj_sudionika,
+    dohvati_sudionika,
+    nadopuni_sudionika,
+    obrisi_sudionika,
+    potrazi_sudionika,
 )
 
 router = APIRouter()
@@ -21,19 +25,97 @@ class Sudionik(BaseModel):
     tel: str
 
 
-@router.get("/{uuid}")
-def read_sudionik(uuid: UUID):
-    return {"UUID": uuid, "Podaci": 'Nema zapisa'}
+class SudionikNone(BaseModel):
+    ime: str = None
+    prezime: str = None
+    email: str = None
+    tel: str = None
+
+
+class SudionikUUID(Sudionik):
+    uuid: UUID
+
+    class Config:
+        orm_mode = True
+
+
+class SudionikSearchResults(BaseModel):
+    ukupno_rezultata: int
+    sudionici: List[SudionikUUID]
+
+    class Config:
+        orm_mode = True
+
+
+@router.get('/search', response_model=SudionikSearchResults)
+async def search_sudionik(
+    prezime: str = Query(..., min_length=1),
+    page: int = Query(1, gt=0),
+    limit: int = Query(1, gt=0),
+    db: Session = Depends(get_db)
+) -> SudionikSearchResults:
+    try:
+        ukupno, sudionici = potrazi_sudionika(
+            db, prezime, page, limit
+        )
+    except Exception as e:
+        raise e
+    return {'ukupno_rezultata': ukupno, 'sudionici': sudionici}
+
+
+@router.get("/{uuid}", response_model=SudionikUUID)
+async def read_sudionik(
+    uuid: UUID,
+    db: Session = Depends(get_db)
+):
+    try:
+        sudionik = dohvati_sudionika(db, uuid)
+    except NoResultFound:
+        raise HTTPException(status_code=404,
+                            detail=f"Sudionik s UUID: {uuid} ne postoji!")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=e)
+    del sudionik.id
+    return sudionik
 
 
 @router.put("/{uuid}")
-def update_sudionik(uuid: UUID, sudionik: Sudionik):
-    return {
-        "UUID": uuid,
-        "Sudionik": f'{sudionik.ime} {sudionik.prezime}',
-        "e-mail": sudionik.email,
-        'telefon': sudionik.tel
-    }
+async def update_sudionik(
+    uuid: UUID,
+    sudionik: SudionikNone,
+    db: Session = Depends(get_db)
+):
+    try:
+        sudionik = nadopuni_sudionika(db, uuid, **sudionik.__dict__)
+    except NoResultFound:
+        raise HTTPException(status_code=404,
+                            detail=f"Sudionik s UUID: {uuid} ne postoji!")
+    except IntegrityError:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Sudionik s email adresom: {sudionik.email} već postoji!"
+        )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=e)
+    del sudionik.id
+    return sudionik
+
+
+@router.post("/", status_code=201)
+async def create_sudionik(
+    sudionik: Sudionik,
+    db: Session = Depends(get_db)
+):
+    try:
+        sudionik = kreiraj_sudionika(db, **sudionik.__dict__)
+    except IntegrityError:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Sudionik s email adresom: {sudionik.email} već postoji!"
+        )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=e)
+    return {"UUID": sudionik.uuid}
 
 
 @router.delete("/{uuid}", status_code=204)
--- dcdb/api.py
+++ dcdb/api.py
@@ -45,3 +45,78 @@ def kreiraj_sudionika(
     return sudionik
 
 
+def dohvati_sudionika(
+    db: Session,
+    uuid: UUID
+):
+    try:
+        sudionik = db.query(Sudionik).filter_by(uuid=uuid).one()
+    except Exception:
+        raise
+    return sudionik
+
+
+def nadopuni_sudionika(
+    db: Session,
+    uuid: UUID,
+    ime: str = None,
+    prezime: str = None,
+    email: str = None,
+    tel: str = None
+):
+    try:
+        sudionik = db.query(Sudionik).filter_by(uuid=uuid).one()
+    except Exception:
+        raise
+    if ime:
+        sudionik.ime = ime
+    if prezime:
+        sudionik.prezime = prezime
+    if email:
+        sudionik.email = email
+    if tel:
+        sudionik.tel = tel
+    try:
+        db.commit()
+    except Exception:
+        db.rollback()
+        raise
+    return sudionik
+
+
+def obrisi_sudionika(
+    db: Session,
+    uuid: UUID,
+):
+    try:
+        sudionik = db.query(Sudionik).filter_by(uuid=uuid).one()
+        db.delete(sudionik)
+        db.commit()
+    except Exception:
+        db.rollback()
+        raise
+    return True
+
+
+def potrazi_sudionika(
+    db: Session,
+    prezime: str,
+    page: int = 1,
+    limit: int = 20
+):
+    offset = (page - 1) * limit
+    try:
+        result = (
+            db.query(Sudionik.ime, Sudionik.prezime,
+                     Sudionik.email, Sudionik.tel, Sudionik.uuid)
+              .select_from(Sudionik)
+              .filter(Sudionik.prezime.ilike(f'{prezime}%'))
+              .order_by(func.similarity(Sudionik.prezime, prezime).desc())
+              .limit(limit).offset(offset).all()
+        )
+        query = db.query(func.count(Sudionik.id))\
+                  .filter(Sudionik.prezime.ilike(f'{prezime}%'))
+        count = query.scalar()
+    except NoResultFound:
+        return False
+    return count, result
